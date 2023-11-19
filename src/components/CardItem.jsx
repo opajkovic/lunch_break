23edc6f96595c433a913af9d8259d21a917ff525
opajkovic <oljaolja170@gmail.com>
2023-11-19 22:03:18 +0100

    added meal photo

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,15 +1,16 @@
-
+import photo from '../assets/meal.jpg'
 
 const CardItem = ({meal}) => {
   const {name,price} = meal
   return (
     <div className="card w-64 mb-4 bg-base-100 shadow-xl">
     
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="meal" /></figure>
+  <figure><img src={photo} alt="meal" /></figure>
   <div className="card-body">
     <h2 className="card-title">{name}!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
+    <p>Lamb chops are usually marinated in various combinations of olive oil, and a variety of fresh herbs before </p>
+    <div className="card-actions justify-between">
+      <span className='text-xl p-2 text-primary'>{price}€</span>
       <button className="btn btn-primary">Buy Now</button>
     </div>
   </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Header, Menu } from "../components";
-import { Cart, Meals } from "./";
+import { Cart } from "./";
 
 const Home = () => {
   return (
